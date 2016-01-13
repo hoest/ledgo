b8bdfb6112c792edd78175fe0fb6baeebc2476cc
Jelle de Jong <jelle@hoest.nl>
2016-01-13 15:07:03 +0100

    Mobiele weergaven tunen

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,11 @@ $(function() {
   $('.ledgo-logo').css('cursor', 'pointer').off('click').on('click', function() {
     location.href = '/';
   });
+
+  $('.mobile-menu i').off('click').on('click', function() {
+    var nav = $(this).closest('header').find('nav');
+    var height = $(window).height() - $("header").height();
+    nav.height(height).css("overflow-y", "auto");
+    nav.slideToggle();
+  });
 });

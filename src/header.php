1cff7d535797ceeafeec2dafbc96f9a1bdf4bd7d
Jelle de Jong <jelle@hoest.nl>
2016-01-13 15:06:40 +0100

    Menu icoon toegevoegd voor mobiele weergave

--- src/header.php
+++ src/header.php
@@ -50,7 +50,7 @@
 
     <?php wp_head(); ?>
 
-    <script>
+    <!--<script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -58,7 +58,7 @@
 
       ga('create', 'UA-188573-9', 'auto');
       ga('send', 'pageview');
-    </script>
+    </script>-->
   </head>
   <body class="<?php if(is_admin_bar_showing()) { ?>adminbar-showing<?php } ?>">
     <div class="wrapper">
@@ -69,6 +69,9 @@
         <nav class="animate fadeIn one">
           <?php wp_nav_menu(array('container' => '', 'theme_location' => 'primary')); ?>
         </nav>
+        <div class="mobile-menu">
+          <i class="fa fa-bars"></i>
+        </div>
         <div class="payoff animate fadeIn two">
           <div class="inner"><?php bloginfo('description'); ?></span>
         </div>

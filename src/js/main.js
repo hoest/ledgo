583f5e54bea322aca503399080a6ffe8fd8c6f5d
Jelle de Jong <jelle@hoest.nl>
2016-01-24 14:11:14 +0100

    Nieuwe 3D perspectief als achtergrond ipv roterende beelden

--- src/js/main.js
+++ src/js/main.js
@@ -15,28 +15,4 @@ $(function() {
     nav.height(height).css('overflow-y', 'auto');
     nav.slideToggle();
   });
-
-  if(!window.templateUrl) {
-    window.templateUrl = '';
-  }
-
-  var bgImages = [
-    window.templateUrl + '/img/01_Signage_Web.jpg',
-    window.templateUrl + '/img/02_Perfume_Web.jpg',
-    window.templateUrl + '/img/03_Information_Web.jpg',
-    window.templateUrl + '/img/04_Entertainment_Web.jpg',
-    window.templateUrl + '/img/05_Architecture_Web.jpg',
-  ];
-
-  var bgImage = new Image();
-  bgImage.src = bgImages[Math.floor(Math.random() * bgImages.length)];
-  random = 'url(' + bgImage.src + ')';
-  $('main').css('background-image', random);
-
-  setInterval(function() {
-    var bgImage = new Image();
-    bgImage.src = bgImages[Math.floor(Math.random() * bgImages.length)];
-    var random = 'url(' + bgImage.src + ')';
-    $('main').css('background-image', random);
-  }, 5000);
 });

c691e713f05f19492297664bb5b027243f6b3812
Jelle de Jong <jelle@hoest.nl>
2016-01-24 14:11:53 +0100

    On resize ook even juiste maten vullen, indien nodig

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 $(function() {
-  wHeight = $(window).outerHeight();
-  mainHeight = $('main').outerHeight() + 35 + 25;
-  if(wHeight > mainHeight) {
-    $('main, footer').addClass('fixed');
+  function resize() {
+    wHeight = $(window).outerHeight();
+    mainHeight = $('main').outerHeight() + 35 + 25;
+    if(wHeight > mainHeight) {
+      $('main, footer').addClass('fixed');
+    }
   }
+
+  resize();
+  $(window).off('resize').on('resize', resize);
 
   $('.ledgo-logo').css('cursor', 'pointer').off('click').on('click', function() {
     location.href = '/';

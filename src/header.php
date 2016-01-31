4ad28ec39660af18f0bc26659ba8c4f5effbd3c2
Jelle de Jong <jelle@hoest.nl>
2016-01-31 12:55:55 +0100

    Analytics added

--- src/header.php
+++ src/header.php
@@ -61,6 +61,14 @@
     </script>-->
   </head>
   <body class="<?php if(is_admin_bar_showing()) { ?>adminbar-showing<?php } ?>">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-73111247-1', 'auto');
+      ga('send', 'pageview');
+    </script>
     <div class="wrapper">
       <header>
         <h1 class="site-title">
